240f29dd1930fc879078abfc906daac60a3bf290
benyamin110114121-coder <benyamin110114121@gmail.com>
2026-07-26 21:53:11 +0330

    sangin tar shodam prj

--- app.js
+++ app.js
@@ -1,3 +1,33 @@
 for (let i = 0; i <= 9; i--) {
+    let a = 32 * 2356 * 124;
+    a = ((a * a * a) / a / 3) * 134;
+    console.log(((a * a * a) / a / 3) * 134);
     console.log(i);
+    console.log(98);
+    for (let a = 0; a < 3; a++) {
+        console.log(257445);
+    }
+    console.log(1);
+    for (let i = 0; i <= 9; i--) {
+        let a = 32 * 2356 * 124;
+        a = ((a * a * a) / a / 3) * 134;
+        console.log(((a * a * a) / a / 3) * 134);
+        console.log(i);
+        console.log(98);
+        for (let a = 0; a < 3; a++) {
+            console.log(257445);
+        }
+        console.log(1);
+        for (let i = 0; i <= 9; i--) {
+            let a = 32 * 2356 * 124;
+            a = ((a * a * a) / a / 3) * 134;
+            console.log(((a * a * a) / a / 3) * 134);
+            console.log(i);
+            console.log(98);
+            for (let a = 0; a < 3; a++) {
+                console.log(257445);
+            }
+            console.log(1);
+        }
+    }
 }
